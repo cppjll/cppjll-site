ab3cf3ff5583bfabc82376fb1a924f4a35d8387f
mikumino <36429067+mikumino@users.noreply.github.com>
2023-10-31 17:15:19 -0700

    BoardMemberCard props

--- src/components/BoardMemberCard.tsx
+++ src/components/BoardMemberCard.tsx
@@ -1,16 +1,21 @@
 import nijika from '../images/nijika look.png'
 
-export default function BoardMemberCard() {
+interface Props {
+    name: string;
+    position: string;
+    image: string;
+}
+
+export default function BoardMemberCard({name, position, image}: Props) {
     return (
         <div className="flex items-center w-full h-48 rounded-xl bg-secondary mb-4">
             <div className="grid grid-cols-2 w-full h-full items-center">
-                <div className='flex align- ml-4'>
-                    {/* <img className="w-24 h-24 ml-4" src="https://picsum.photos/200" alt="board member" /> */}
-                    <img src={nijika.src} alt="board member" />
+                <div className='flex align- ml-6 w-36'>
+                    <img src={image} alt="board member" className='rounded-full' />
                 </div>
-                <div className="text-right mr-4" >
-                    <h1 className="text-3xl text-accent font-display font-bold">Nijika Ijichi</h1>
-                    <p className="text-lg text-accent">President</p>
+                <div className="text-right mr-6" >
+                    <h1 className="text-3xl text-accent font-display font-bold">{name}</h1>
+                    <p className="text-lg text-accent">{position}</p>
                 </div>
             </div>
         </div>
