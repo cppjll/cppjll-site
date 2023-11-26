98a2dcbd2ff800ea48885d32d46ea8d3f636da38
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-26 13:45:11 -0800

    Added site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cpp-jll.com',
   integrations: [react(), tailwind(), sitemap()]
 });
