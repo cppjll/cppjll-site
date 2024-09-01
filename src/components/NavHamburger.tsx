3f34f7da050ec65268ebbbfbefc1fd85dda0cbf9
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-09-01 13:36:36 -0700

    new hero, navbar

---
+++ src/components/NavHamburger.tsx
@@ -0,0 +1,37 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+export default function NavHamburger() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button className="md:hidden absolute right-4 top-2" variant="ghost">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>CPP JLL</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col mt-8">
+          <a
+            className={`${buttonVariants({ variant: "ghost" })} text-lg`}
+            href="/events"
+          >
+            Events
+          </a>
+          <a className={buttonVariants({ variant: "ghost" })} href="/board">
+            Board
+          </a>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
