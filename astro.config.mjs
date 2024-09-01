40cdf1524289a23dcf3d0f2438703086e8f57de4
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-09-01 11:18:17 -0700

    shadcn init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
 import sitemap from "@astrojs/sitemap";
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,55 +1,95 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-        screens: {
-            'xsm': '435px',
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
-            '2xl': '1536px'
-        },
-		extend: {
-            colors:{
-                // Instagram Color Gradient (Orange, Red, Purple)
-                'igOrange': '#f9ce34',
-                'igRed': '#ee2a7b',
-                'igPurple': '#6228d7',
-                // Discord Blurple
-                'blurple': '#7289da',
-            },
-            fontFamily: {
-                'sans': ['"DM Sans"', 'sans-serif'],
-                'display': ['"DM Serif Display"', 'serif'],
-            },
-            fontSize:{
-                // Font size specifically for navbar
-                'nv': ['1.25rem', {
-                    lineHeight:1,
-                }],
-            },
-            typography: {
-                DEFAULT: {
-                    css: {
-                        color: '#204063',
-                        h1: {
-                            color: '#204063',
-                        },
-                        h2: {
-                            color: '#204063',
-                        },
-                        h3: {
-                            color: '#204063',
-                        },
-                        strong: {
-                            color: '#204063',
-                        },
-                    },
-                },
-            },
-        },
-	},
+    	screens: {
+    		xsm: '435px',
+    		sm: '640px',
+    		md: '768px',
+    		lg: '1024px',
+    		xl: '1280px',
+    		'2xl': '1536px'
+    	},
+    	extend: {
+    		colors: {
+    			igOrange: '#f9ce34',
+    			igRed: '#ee2a7b',
+    			igPurple: '#6228d7',
+    			blurple: '#7289da',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		fontFamily: {
+    			sans: [DM Sans", sans-serif],\n    			display: [DM Serif Display", serif]
+    		},
+    		fontSize: {
+    			nv: [1.25rem, {\\r\\n                    lineHeight:1,\\r\\n                }]
+    		},
+    		typography: {
+    			DEFAULT: {
+    				css: {
+    					color: '#204063',
+    					h1: {
+    						color: '#204063'
+    					},
+    					h2: {
+    						color: '#204063'
+    					},
+    					h3: {
+    						color: '#204063'
+    					},
+    					strong: {
+    						color: '#204063'
+    					}
+    				}
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
+    },
 
     daisyui: {
         themes: [
@@ -66,5 +106,6 @@ export default {
 	plugins: [
         require('@tailwindcss/typography'),
         require("daisyui"),
+        require("tailwindcss-animate")
     ],
 }
